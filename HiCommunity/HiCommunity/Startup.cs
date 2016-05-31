24d9cffeb49263bb87f1295edafc3f44ed8a4b21
wangyuhang <wangyuhang6688@gmail.com>
2016-05-31 14:59:27 +0800

    chu shi hua  hai shq qu

---
+++ HiCommunity/HiCommunity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HiCommunity.Startup))]
+namespace HiCommunity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ HiCommunity/HiCommunityDBAccessor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiCommunityDBAccessor
+{
+    public class Class1
+    {
+    }
+}
